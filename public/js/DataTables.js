635f841dc827057bb6c5717b634ee12b9765352d
nikidev <nikolay.mikov1@gmail.com>
2016-05-15 23:48:02 +0300

    datatables search and sorting modifying

--- public/js/DataTables.js
+++ public/js/DataTables.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
-    $('#tblPhones').DataTable({
-    	"pageLength": 4
+   var table = $('#tblPhones').DataTable({
+    	
+    	"aoColumnDefs": [
+          { 'bSortable': false, 'aTargets': [ 0,2,3,4,5,6 ] }
+       ],
+
+       "pageLength": 4
     });
+
+    $('#search').on('keyup',function(){
+      table.search($(this).val()).draw() ;
+	});
 });
